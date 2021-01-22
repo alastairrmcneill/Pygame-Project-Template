7fb84b62ddc5d9cb8c46605396f81fec69cad8f7
Ali <alastair.r.mcneill@gmail.com>
2021-01-22 22:52:15 +0000

    First commit

---
+++ Components/Constants.py
@@ -0,0 +1,16 @@
+"""
+Constants for the game
+"""
+
+import pygame
+pygame.font.init()
+
+# Window variables
+WIN_WIDTH = 600
+WIN_HEIGHT = 600
+
+# Colours
+
+
+# Fonts
+
---
+++ Main.py
@@ -0,0 +1,36 @@
+import pygame
+from Scenes.Manager import Manager
+from Components.Constants import WIN_WIDTH, WIN_HEIGHT
+
+WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("REPLACE ME")
+
+scene_dict = {}             # Dict {"Name": Scene_Class()}
+starting_scene = ""         # Name of the staring scene
+
+def main():
+    clock = pygame.time.Clock()
+    running = True
+
+    manager = Manager()
+    manager.setup_scenes(scene_dict, starting_scene)
+
+    while running:
+        clock.tick(60)
+
+        if manager.scene.done:
+            manager.swap_scenes()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                return
+            manager.scene.handle_event(event)
+
+        manager.scene.update()
+        manager.scene.draw(WIN)
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
