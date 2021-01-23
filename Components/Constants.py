6e8aa0c15da62dcd1991bf01e83a556bc747d503
alastairrmcneill <52216324+alastairrmcneill@users.noreply.github.com>
2021-01-23 11:32:53 +0000

    Update Constants.py

--- Components/Constants.py
+++ Components/Constants.py
@@ -1,7 +1,7 @@
 """
 Constants for the game
 """
-
+import os
 import pygame
 pygame.font.init()
 
@@ -12,6 +12,9 @@ WIN_HEIGHT = 600
 # Colours
 
 # Paths
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+IMGS_PATH = os.path.join(BASE_PATH, "Imgs")
+FONTS_PATH = os.path.join(BASE_PATH, "Fonts")
 
 # Images
 
